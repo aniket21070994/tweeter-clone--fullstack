2f51517c4ed748615e8a7f207270104f612c9320
aniket21070994 <aniket.mankar001@gmail.com>
2024-06-04 20:22:36 +0530

    backend  stage 1 completed with basic user support api's

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config(
     {
-        path:"../config/.env"
+        path: "../config/.env"
     }
 )
-const dbconnect=()=>{
+const dbconnect = () => {
     console.log(process.env.MONGO_URI)
-    mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Connected to DB")).catch((err)=>console.log(err))
+    mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to DB")).catch((err) => console.log(err))
 }
 export default dbconnect;
