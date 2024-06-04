4277e877966a72c03628097b53a1c6e5d7b608c3
aniket21070994 <aniket.mankar001@gmail.com>
2024-06-05 00:12:59 +0530

    toast added for good  ui updates

--- socialmedia/src/App.jsx
+++ socialmedia/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Body from './components/Body';
 import Home from './components/Home';
+import {Toaster} from "react-hot-toast"
 
-function App()
-  {
-      
-    return(
-      <div>
-        <Body/>
-      </div>
-    );
-  }
+function App() {
+
+  return (
+    <div>
+      <Body />
+      <Toaster/>
+    </div>
+  );
+}
 export default App;
