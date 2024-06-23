90f4bbc1de750bbfa882b9cab25de979ce7c260f
aniket21070994 <aniket.mankar001@gmail.com>
2024-06-23 17:15:10 +0530

    update cors site

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://tweeter-clone-fullstack-1.onrender.com",credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 
